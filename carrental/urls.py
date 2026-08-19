c76d3c2671a7461083364bd14ed348afefd62848
ITDani-dot <dannymana2@gmail.com>
2026-08-19 19:09:29 +0200

    Remove duplicate static media URL configuration

--- carrental/urls.py
+++ carrental/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 # Add this - serve static + media in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
