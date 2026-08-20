217b2bd7ff4675d06e07eab2c0838940329a9c9d
ITDani-dot <dannymana2@gmail.com>
2026-08-20 06:57:37 +0200

    Add media URL handling to urlpatterns

--- carrental/urls.py
+++ carrental/urls.py
@@ -16,3 +16,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # In production Render + Whitenoise handles static
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
