c9f515679b83040a4aa80e0733b0c1f445805a5e
ITDani <itdann12@gmail.com>
2026-08-21 23:41:42 +0200

    Fix context processor path

---
+++ carrental/context_processors.py
@@ -0,0 +1,7 @@
+def company_context(request):
+    return {
+        'company_name': 'SN TRANSPORT',
+        'slogan': 'Your Trusted Partner in Premium Transport',
+        'location': 'Kigali, Rwanda',
+        'phone': '+250 788 123 456',
+    }
